586f690cb2175cb3244d935608b65e72cc316087
ItzYumi <ddumacc001@gmail.com>
2026-08-18 16:11:02 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [10200395747] = "https://raw.githubusercontent.com/ItzYumi/Games/refs/heads/main/Grow%20a%20Garden%202.lua" -- GaG2
+  [10200395747] = "https://raw.githubusercontent.com/ItzYumi/Games/refs/heads/main/Grow%20a%20Garden%202.lua", -- GaG2
+  [10563114921] = "https://raw.githubusercontent.com/ItzYumi/Games/refs/heads/main/Steal%20An%20Egg.lua" -- SaE
 }
 
 return Games
